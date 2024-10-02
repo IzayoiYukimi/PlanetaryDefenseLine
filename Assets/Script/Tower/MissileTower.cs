132de9618e5ea841d151e5a448d8333ef38c8dbf
IzayoiYukimi <hansya0v0@gmail.com>
2024-10-02 16:38:15 +0900

    player prefab

--- Assets/Script/Tower/MissileTower.cs
+++ Assets/Script/Tower/MissileTower.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class MissileTower : MonoBehaviour
 {
+    [SerializeField] GameObject towertop;
+    [SerializeField] float warningradius = 20.0f;
+    [SerializeField] List<GameObject> enemyinwarning;
+    public bool hastarget;
+    public Vector3 targetpos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        towertop = transform.Find("TowerTop").gameObject;
     }
 
     // Update is called once per frame
